40192a36e9d959747d58a8c9aec34a58479e2576
Irwan Hermawan <rwnhermawan@gmail.com>
2024-10-29 15:36:38 +0700

    implement module lesson

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,26 @@
 import { Module } from '@nestjs/common';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { LessonModule } from './lesson/lesson.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import {Lesson} from "./lesson/lesson.entity";
+import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mongodb',
+      url: 'mongodb://localhost/school',
+      synchronize: true,
+      useUnifiedTopology: true,
+      entities: [Lesson],
+    }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+    }),
+    LessonModule,
+    StudentModule,
+  ],
 })
 export class AppModule {}
